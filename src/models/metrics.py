2ddf9e21d382491c91390bfe56158cf3f31950e4
Miguel FMC <miguelfm95@gmail.com>
2019-04-02 17:05:26 +0200

    Precision, recall and f1score metrics

---
+++ src/models/metrics.py
@@ -0,0 +1,35 @@
+"""Defines metrics
+"""
+
+import tensorflow.keras.backend as K 
+
+def precision(y_true, y_pred):
+    """Precision metric for keras model
+    """
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
+    precision = true_positives / (predicted_positives + K.epsilon())
+    return precision
+
+
+def recall(y_true, y_pred):
+    """Recall metric for keras model
+    """
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    total_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
+    recall = true_positives / (total_positives + K.epsilon())
+    return recall
+
+
+def f1_score(y_true, y_pred):
+    """Computes the F score for keras model
+    The F score is the weighted harmonic mean of precision and recall.
+    """
+    # If there are no true positives, fix the F score at 0 like sklearn.
+    if K.sum(K.round(K.clip(y_true, 0, 1))) == 0:
+        return 0
+
+    p = precision(y_true, y_pred)
+    r = recall(y_true, y_pred)
+    f1_score = 2 * (p * r) / (p + r + K.epsilon())
+    return f1_score
