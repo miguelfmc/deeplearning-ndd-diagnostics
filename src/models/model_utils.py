286e5fc4fc97469f0c2a078d0fa5316dd4d88114
Miguel FMC <miguelfm95@gmail.com>
2019-04-08 19:06:15 +0200

    Included scalograms dataset. Split without mixing

--- src/models/model_utils.py
+++ src/models/model_utils.py
@@ -9,27 +9,58 @@ from tensorflow.keras.models import model_from_json
 import pickle
 
 
-def load_dataset():
-    in_dir = os.path.join('data', 'processed')
+def load_dataset(dataset_name='spectrograms-dataset', mix=True):
+    """Loads one of the datasets: spectrograms or scalograms,
+    either with mix of patients between the train and dev sets or not
 
-    train_data = np.load(os.path.join(in_dir, 'train.npz'))
+    Arguments
+        dataset_name: string defining the dataset
+        mix: boolean that indicates if a mixed train-dev set should be loaded or not
+            if set to True records from the same patients appear in both training and dev set
+            if set to False 4 the dev set will be comprised of records from 4 patients that
+                will not appear in the train set
+    
+    Returns
+        X (images), Y (labels), Z (other info) of train, dev and test datasets as numpy ndarrays
+    """
+    in_dir = os.path.join('data', 'processed', dataset_name)
+
+    if mix:
+        train_data = np.load(os.path.join(in_dir, 'train-dev.npz'))
+
+        X_orig = train_data['X_train']
+        Y_orig = train_data['Y_train']
+        Z_orig = train_data['Z_train']
+
+        X_orig = X_orig.reshape(X_orig.shape[0], X_orig.shape[1], X_orig.shape[2], 1)
+
+        # random state seed to always keep the same split
+        X_train_orig, X_dev_orig, Y_train_orig, Y_dev_orig, Z_train_orig, Z_dev_orig = train_test_split(
+            X_orig, Y_orig, Z_orig, random_state=1, test_size=0.20)
+    else:
+        train_data = np.load(os.path.join(in_dir, 'train.npz'))
+        dev_data = np.load(os.path.join(in_dir, 'dev.npz'))
+
+        X_train_orig = train_data['X_train']
+        Y_train_orig = train_data['Y_train']
+        Z_train_orig = train_data['Z_train']
+
+        X_train_orig = X_train_orig.reshape(X_train_orig.shape[0], X_train_orig[1], X_train_orig.shape[2], -1)
+
+        X_dev_orig = dev_data['X_dev']
+        Y_dev_orig = dev_data['Y_dev']
+        Z_dev_orig = dev_data['Z_dev']
+
+        X_dev_orig = X_dev_orig.reshape(X_dev_orig.shape[0], X_dev_orig[1], X_dev_orig.shape[2], -1)
+
+    # test data always the same
     test_data = np.load(os.path.join(in_dir, 'test.npz'))
 
-    X_orig = train_data['X_train']
-    Y_orig = train_data['Y_train']
-    Z_orig = train_data['Z_train']
     X_test_orig = test_data['X_test']
     Y_test_orig = test_data['Y_test']
     Z_test_orig = test_data['Z_test']
 
-    X_orig = X_orig.reshape(X_orig.shape[0], X_orig.shape[1],
-                            X_orig.shape[2], 1)
-    X_test_orig = X_test_orig.reshape(X_test_orig.shape[0], X_test_orig.shape[1],
-                            X_test_orig.shape[2], 1)
-
-    # random state seed to always keep the same split
-    X_train_orig, X_dev_orig, Y_train_orig, Y_dev_orig, Z_train_orig, Z_dev_orig = train_test_split(
-        X_orig, Y_orig, Z_orig, random_state=1, test_size=0.20)
+    X_test_orig = X_test_orig.reshape(X_test_orig.shape[0], X_test_orig.shape[1], X_test_orig.shape[2], 1)
 
     return X_train_orig, Y_train_orig, Z_train_orig, X_dev_orig, Y_dev_orig, Z_dev_orig, X_test_orig, Y_test_orig, Z_test_orig
 
@@ -89,13 +120,29 @@ def redefine_labels(Y, target_class):
         then Y_binary[i] is 1. Otherwise, Y[i] is 0
     
     Arguments
-    Y -- A (m, 1) NumPy array of labels ranging from 0 to (K - 1) with K being the number of classes
-    target_class -- Integer indicating the target class
+        Y: a (m, 1) NumPy array of labels ranging from 0 to (K - 1) with K being the number of classes
+        target_class: integer indicating the target class
     
     Returns
-    Y_binary -- A binary labelled (0 or 1) NumPy array of the same shape as Y
+        Y_binary: a binary labelled (0 or 1) NumPy array of the same shape as Y
     """
     
     Y_binary = (Y == target_class).astype(int)
     
     return Y_binary
+
+
+def standardize_per_example(X):
+    """
+    Standardizes a numpy ndarray by dividing each example by its range
+
+    Arguments
+        X: numpy ndarray of dimensions (m, h, w, c)
+    
+    Returns:
+        The standardized array of same dimensions
+    """
+    maxes = X.max(axis=(1,2,3), keepdims=True)
+    mins = X.min(axis=(1,2,3), keepdims=True)
+    
+    return X / (maxes - mins)
